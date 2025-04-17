4c8deb02f1674d826ca880dc467127f05960412d
abcdxyz-sad <kle61826@gmail.com>
2025-04-17 19:24:20 +0700

    Troi oi met muon chet

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ app.use('/chude', chudeRouter);
 app.use('/taikhoan', taikhoanRouter);
 app.use('/baiviet', baivietRouter);
 
-app.listen(3000, () => {
+app.listen(3000, '216.24.60.0/24', () => {
 	console.log('Server is running at 216.24.60.0/24');
 });
