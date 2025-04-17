832dc0b163bb6f8198ebe3a24b386587da6b3e0a
abcdxyz-sad <kle61826@gmail.com>
2025-04-17 19:04:30 +0700

    Troi oi chay di lam on

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ app.use('/chude', chudeRouter);
 app.use('/taikhoan', taikhoanRouter);
 app.use('/baiviet', baivietRouter);
 
-app.listen(3000, () => {
-	console.log('Server is running at http://127.0.0.1:3000');
-});
+/*app.listen(3000, () => {
+	console.log('Server is running at 216.24.60.0/24');
+});*/
